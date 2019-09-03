acc94e357a7a28fd948b98f7754608f6a8d4c0d9
Ali Kadir BAGCIOGLU <alikadir.bagcioglu@digiturk.com.tr>
2019-09-03 11:36:26 +0300

    init

---
+++ FirstDotnetCoreMVC/FirstDotnetCoreMVC/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FirstDotnetCoreMVC.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
---
+++ FirstDotnetCoreMVC/FirstDotnetCoreMVC/Models/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FirstDotnetCoreMVC.Models
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        DbSet<Employee> Employees { get; set; }
+    }
+}
