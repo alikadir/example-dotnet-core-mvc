070cae65bd38458baf3b3a9031c1bf8020392f21
Ali Kadir BAGCIOGLU <alikadir.bagcioglu@digiturk.com.tr>
2019-10-08 16:16:22 +0300

    EmailTagHelper

---
+++ TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace FirstDotnetCoreMVC.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string MailTo { get; set; }
+        public string Domain { get; set; } = "sorburaya.com";
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; // Replaces <email> with <a> tag
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + Domain;
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
